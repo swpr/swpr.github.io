12c535a3243019e93a5cddc4810b389778f975bf
swpr <57578258+swpr@users.noreply.github.com>
2020-12-21 19:21:27 +0800

    Update a.js

--- a.js
+++ a.js
@@ -5,4 +5,5 @@ var link = ["https://ce.ayrce.com/?xxxsas=1" , "https://bz.lynsr.info/?r=1" , "h
 var img = ["https://swpr.github.io/sp2.gif" , "https://swpr.github.io/grea.gif" , "https://swpr.github.io/sp.gif", "https://swpr.github.io/0.gif", "https://swpr.github.io/1.gif", "https://swpr.github.io/2.gif", "https://swpr.github.io/3.gif", "https://swpr.github.io/4.gif", "https://swpr.github.io/5.gif", "https://swpr.github.io/7.gif", "https://swpr.github.io/8.gif", "https://swpr.github.io/9.gif", "https://swpr.github.io/a.gif", "https://swpr.github.io/b.gif", "https://swpr.github.io/c.gif", "https://swpr.github.io/d.gif", "https://swpr.github.io/e.gif", "https://swpr.github.io/f.gif", "https://swpr.github.io/g.gif", "https://swpr.github.io/h.gif", "https://swpr.github.io/i.gif", "https://swpr.github.io/j.gif"];
 
 var randomLink = link[Math.floor(Math.random() * link.length)]; 
+var randomImage= img[Math.floor(Math.random() * img.length)];
 document.write('<a target=_blank href="' + randomLink + '"><img src="'+randomImage+'"></a>'); 
