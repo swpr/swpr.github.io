057790072ccfa1db78fad23d573ca6b6f8382cee
swpr <57578258+swpr@users.noreply.github.com>
2020-12-21 19:16:19 +0800

    Create a.js

---
+++ a.js
@@ -0,0 +1,8 @@
+
+
+
+var link = ["https://ce.ayrce.com/?xxxsas=1" , "https://bz.lynsr.info/?r=1" , "https://tr.ivyrc.com/?avclipx=1" , "https://he.lynsr.info/?herg=1","https://th.lynsr.info/?e8a=1","https://op.nyrvc.com/?r=1","https://gf.ivyrc.com/?c4a=1","https://ng.lynsr.info/?r=1","https://ft.nyrvc.com/?e3d=1","https://du.lynsr.info/?b9c=1","https://ye.swpra.com/?r=1","https://su.ivyrc.com/?xb9=1","https://tn.ayrce.com/?xxxp7=1","https://ya.swpra.com/?r=1","https://ct.alrau.com/?r=1","https://az.alrau.com/?r=1","https://pe.swpra.com/?r=1","https://zi.swpra.com/?r=1","https://dc.swpra.com/?r=1","https://ix.swpra.com/?r=1"];
+var img = ["https://swpr.github.io/sp2.gif" , "https://swpr.github.io/grea.gif" , "https://swpr.github.io/sp.gif", "https://swpr.github.io/0.gif", "https://swpr.github.io/1.gif", "https://swpr.github.io/2.gif", "https://swpr.github.io/3.gif", "https://swpr.github.io/4.gif", "https://swpr.github.io/5.gif", "https://swpr.github.io/7.gif", "https://swpr.github.io/8.gif", "https://swpr.github.io/9.gif", "https://swpr.github.io/a.gif", "https://swpr.github.io/b.gif", "https://swpr.github.io/c.gif", "https://swpr.github.io/d.gif", "https://swpr.github.io/e.gif", "https://swpr.github.io/f.gif", "https://swpr.github.io/g.gif", "https://swpr.github.io/h.gif", "https://swpr.github.io/i.gif", "https://swpr.github.io/j.gif"];
+
+var randomLink = link[Math.floor(Math.random() * link.length)]; 
+document.write('<a target=_blank href="' + randomLink + '"><img src="'+randomImage+'"></a>'); 
